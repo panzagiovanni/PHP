a570f6e7fe7cfd79150c77dbaac74a9b80527299
Giovanni Panza <panzagiovanni@gmail.com>
2023-03-03 00:23:54 +0100

    Update listaprenotazioni.php

--- Session/prenotazioni/listaprenotazioni.php
+++ Session/prenotazioni/listaprenotazioni.php
@@ -18,6 +18,16 @@
   }
   echo "<br /><b>Totale persone</b> ".$tot."<br />";
   echo "<b>Prenotazioni max:</b> ".$eventomax." posti ".$maxnum."<br />";
+  
+  /*
+  NOTE TO ASSOCIATIVE ARRAY
+  $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+  */
 ?>
 </body>
 </html>
